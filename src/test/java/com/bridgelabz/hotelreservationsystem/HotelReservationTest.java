491f43370a2a90678c76978ebafb25bfb4fda0ee
777shetty <shetty.chetu7@gmail.com>
2021-10-03 22:52:40 +0530

    ADDED:Functionality to validate find cheapest hotel functionality

--- src/test/java/com/bridgelabz/hotelreservationsystem/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/hotelreservationsystem/HotelReservationTest.java
@@ -3,6 +3,7 @@ package com.bridgelabz.hotelreservationsystem;
 
 import org.junit.Assert;
 import org.junit.Test;
+import java.time.LocalDateTime;
 
 public class HotelReservationTest {
 
@@ -30,5 +31,18 @@ public class HotelReservationTest {
         Assert.assertEquals(1, hotelReservation.getSize());
 
 
+    }
+    @Test
+    public void givenStartAndEndDate_ShouldReturnHotelWithCheapestPrice() {
+        HotelReservation hotelReservation = new HotelReservation();
+        hotelReservation.addHotel("LakeWood",3, CustomerType.REGULAR,110);
+        hotelReservation.addHotel("BridgeWood",4, CustomerType.REGULAR,10);
+        hotelReservation.addHotel("RidgeWood",5, CustomerType.REGULAR,220);
+
+
+        LocalDateTime startDate = LocalDateTime.of(2021, 9, 10, 0, 0);
+        LocalDateTime endDate = LocalDateTime.of(2021, 9, 12, 0, 0);
+        Hotel hotel = hotelReservation.getCheapestHotel(startDate, endDate);
+        Assert.assertEquals("BridgeWood", hotel.getHotelName());
     }
 } 
